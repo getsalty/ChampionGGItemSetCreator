9d7ddf63301a6e0cd58afd4a14c83197059a9867
garrett <xelfarcherdv@aol.com>
2015-03-16 14:05:27 -0500

    Updated

--- championGG parser/Champion.cs
+++ championGG parser/Champion.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿//Copyright 2015 Garrett Long
+//This file is part of ChampionGG Item Set Creator.
+//ChampionGG Item Set Creator is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+//Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//ChampionGG Item Set Creator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with ChampionGG Item Set Creator. If not, see http://www.gnu.org/licenses/.
+
+
+
+using System.Collections.Generic;
 
 namespace championGG_parser
 {
